3453787fcda7ec1e3e90de33b73fe28844cc1d9e
fawazbayureksa <fawwazbayureksa@gmail.com>
2026-01-11 12:27:40 +0700

    remove href property from categories tab options

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -96,7 +96,6 @@ export default function TabLayout() {
         name="categories"
         options={{
           title: '',
-          href: null,
           tabBarButton: () => <AddButton />,
         }}
         />
